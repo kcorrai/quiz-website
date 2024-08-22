d287cf84bdb10d8587d776307657bbc4dc50c480
kcorrai <yanlizcakaan@gmail.com>
2024-08-22 11:19:25 +0300

    basic layout

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
